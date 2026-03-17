77db39c144999406caeab226c74ea65bada4f439
deepakkushwaha78 <deepak.kushwaha@opstree.com>
2026-03-17 22:51:13 +0530

    Create locals.tf

---
+++ modules/terraform-aws-ecr/locals.tf
@@ -0,0 +1,17 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  current_account        = format("arn:aws:iam::%s:root", data.aws_caller_identity.current.account_id)
+  only_pull_accounts     = formatlist("arn:aws:iam::%s:root", var.only_pull_accounts)
+  push_and_pull_accounts = formatlist("arn:aws:iam::%s:root", var.push_and_pull_accounts)
+
+  private_repos = {
+    for name, config in var.repo_details :
+    name => config if config.repository_type == "private"
+  }
+
+  public_repos = {
+    for name, config in var.repo_details :
+    name => config if config.repository_type == "public"
+  }
+}
