cc5f3211b425e4d54bdad4415a2d2c0f5b746574
deepakkushwaha78 <deepak.kushwaha@opstree.com>
2026-03-19 17:39:38 +0530

    Update outputs.tf

--- modules/terraform-aws-ecr/outputs.tf
+++ modules/terraform-aws-ecr/outputs.tf
@@ -1,7 +1,7 @@
 output "private_repositories" {
   description = "Map of private repository names to their name, URL, and ARN."
   value = {
-    for name, repo in aws_ecr_repository.private : name => {
+    for name, repo in aws_ecr_repository.ecr_private_repo : name => {
       name = name
       url  = repo.repository_url
       arn  = repo.arn
@@ -12,7 +12,7 @@ output "private_repositories" {
 output "public_repositories" {
   description = "Map of public repository names to their name and URL."
   value = {
-    for name, repo in aws_ecrpublic_repository.public : name => {
+    for name, repo in aws_ecrpublic_repository.ecr_public_repo : name => {
       name = name
       url  = repo.repository_uri
     }
