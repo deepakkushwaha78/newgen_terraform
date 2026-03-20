3dde7023da4c9b169ec2bdd97c46aa9517108880
deepakkushwaha78 <deepak.kushwaha@opstree.com>
2026-03-20 23:37:30 +0530

    Update locals.tf

--- modules/terraform-aws-ecr/locals.tf
+++ modules/terraform-aws-ecr/locals.tf
@@ -15,10 +15,9 @@ locals {
     name => config if config.repository_type == "public"
   }
 
-  common_tags = merge(
-    var.tags,
-    {
-      Customer-Code = var.ecr_tags
-    }
-  )
+  # base_name is derived from the "Name" key in var.tags (set per-resource in tfvars)
+  base_name = lookup(var.tags, "Name", "")
+
+  # common_tags strips the "Name" key so each resource can set its own Name tag
+  common_tags = { for k, v in var.tags : k => v if k != "Name" }
 }
