0be56bad7b6553bc0ed795dc4e376e6a768b7031
deepakkushwaha78 <deepak.kushwaha@opstree.com>
2026-03-20 23:04:10 +0530

    Update locals.tf

--- modules/terraform-aws-secrets-manager/locals.tf
+++ modules/terraform-aws-secrets-manager/locals.tf
@@ -1,8 +1,7 @@
 locals {
-  common_tags = merge(
-    var.tags,
-    {
-      Customer-Code = var.secrets_manager_tags
-    }
-  )
+  # base_name is derived from the "Name" key in var.tags (set per-resource in tfvars)
+  base_name = lookup(var.tags, "Name", "")
+
+  # common_tags strips the "Name" key so each resource can set its own Name tag
+  common_tags = { for k, v in var.tags : k => v if k != "Name" }
 }
